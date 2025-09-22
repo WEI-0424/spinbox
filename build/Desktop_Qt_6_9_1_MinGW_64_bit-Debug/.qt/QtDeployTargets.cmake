daa2786d42ff36b70028be6c49945d8050a47f58
WEI-0424 <41343149@nfu.edu.tw>
2025-09-22 15:05:24 +0800

    project Created!

---
+++ build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_spinbox_FILE C:/Users/user/Desktop/spinbox/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/spinbox.exe)
+set(__QT_DEPLOY_TARGET_spinbox_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_spinbox_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
